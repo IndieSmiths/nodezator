7548d6ebfbc7cc39ec0b4671dc54b38a03c66ddb
Kennedy Richard <kennedy@kennedyrichard.com>
2026-07-18 16:09:54 -0300

    underway: allow user to change fonts
    - added new function to create SVG polygon surfs
    
    (also started replacing render_layered_icon with SVG rendering)

--- nodezator/graphman/callablenode/vizprep/bodysetup/collapsedcreation.py
+++ nodezator/graphman/callablenode/vizprep/bodysetup/collapsedcreation.py
@@ -18,8 +18,6 @@ from .....config import APP_REFS
 from .....surfsman.draw import blit_aligned
 from .....surfsman.render import render_rect
 
-from .....classes2d.single import Object2D
-
 from .....textman.render import render_text
 
 from ...surfs import (
@@ -47,18 +45,24 @@ def get_collapsed_body_surface(self):
     ### reference the body's height locally
     body_height = self.body.rect.height
 
-    ### define color used for body and labels bg, based
-    ### on the "commented out" state
-
-    node_light_bg_color = (
-        COMMENTED_OUT_NODE_BG if self.data.get("commented_out", False) else NODE_BODY_BG
-    )
-
-    ### create a surface for the body of the node
-    body_surf = render_rect(184, body_height, node_light_bg_color)
+    ### reference surface for the body of the node
+    body_surf = self.body.image
 
     ### obtain body head surf from corresponding map
-    BODY_HEAD_SURF = BODY_HEAD_SURFS_MAP[self.category_color]
+
+    BODY_HEAD_SURF = (
+
+        BODY_HEAD_SURFS_MAP[
+
+            (
+                top_rectsman.width,
+                self.title_text_obj.rect.height - top_rectsman.height + 3,
+                self.category_color,
+            )
+
+        ]
+
+    )
 
     ### blit the body head surface in the top of the
     ### body surface; such head surface has a color equal
@@ -66,37 +70,75 @@ def get_collapsed_body_surface(self):
     ### part of the body appear as if merged with its top
 
     blit_aligned(
+
         ## surface to blit
         BODY_HEAD_SURF,
+
         ## target surface
         body_surf,
-        "midtop",  ## retrieve pos from this
-        "midtop",  ## assign pos to this
+
+        'midtop',  ## retrieve pos from this
+        'midtop',  ## assign pos to this
+
     )
 
-    ### create a rect representing the height of text
-    ### surfaces used in the node
-    text_rect = Rect(0, 0, 0, APP_REFS.general_font_height)
+    ### offset for blitting surfaces onto body's surf
+    offset = tuple(-coordinate for coordinate in self.body.rect.topleft)
 
-    ### in order to blit surfaces in the body surface
-    ### relative to the surface's origin, define an offset
-    ### equal to the body's topleft coordinates inverted
-    offset = tuple(-value for value in self.body.rect.topleft)
-
-    ### reference lists of visible input and output sockets locally
-
-    vis = self.visible_input_sockets
+    ### reference list of visible output sockets locally
     vos = self.visible_output_sockets
 
-    ### reference input socket map locally for easier/quick access
+    ### blit labels on the body surface, beside each output
+    ### socket so the name of each output socket is visible
+    ### on the node
+
+    ## retrieve names of outputs in the order they appear
+    ordered_output_names = self.ordered_output_type_map.keys()
+
+    ## reference map from where to retrive output sockets
+    osl_map = self.output_socket_live_map
+
+    ## reference the output text object map locally
+    oto_map = self.output_text_obj_map
+
+    ## iterate over names of outputs, blitting the text surface for each name
+    ## on the body surface
+
+    for output_name in ordered_output_names:
+        
+        ## skip if respective socket is not visible
+
+        if osl_map[output_name] not in vos:
+            continue
+
+        ## draw
+
+        text_obj = oto_map[output_name]
+        text_rect = text_obj.rect
+
+        _topleft = text_rect.topleft
+
+        text_rect.move_ip(offset)
+
+        text_obj.draw_on_surf(body_surf)
+
+        text_rect.topleft = _topleft
+
+
+    ### iterate over the name of each parameter, blitting text surfaces
+    ### representing them on the body of the node in the appropriate locations
+
+    ## reference input socket map locally for easier/quick access
     isl_flmap = self.input_socket_live_flmap
 
-    ### reference unpacking icon map locally
+    ## reference unpacking icon map locally
     sui_flmap = self.subparam_unpacking_icon_flmap
 
-    ### iterate over the name of each parameter, blitting
-    ### text surfaces representing them on the body of
-    ### the node in the appropriate locations
+    ## reference list of visible output sockets locally
+    vis = self.visible_input_sockets
+
+    ## reference the parameter text object map locally
+    pto_map = self.parameter_text_obj_map
 
     parameters_names = self.signature_obj.parameters.keys()
 
@@ -105,14 +147,13 @@ def get_collapsed_body_surface(self):
         ### retrieve the rectsman of the parameter
         param_rectsman = self.param_rectsman_map[param_name]
 
-        ### position the text rect in the appropriate place
-        ### so we can use its topleft as the spot wherein
-        ### to blit the surface we're about to create;
+
+        ### for each parameter with visible input socket(s),
+        ### temporarily position text object relative to node's body
+        ### and draw it;
         ###
-        ### in this part we also define the text to be used
-        ### for the parameter (either its own name or
-        ### its name slightly altered if it is a parameter
-        ### of variable kind)
+        ### in the case of subparameters, also draw the unpacking icon if/where
+        ### needed;
 
         ## try retrieving the variable kind of the parameter
         try:
@@ -124,25 +165,23 @@ def get_collapsed_body_surface(self):
         except KeyError:
 
             ## skip this parameter if it's input socket is not visible
-            input_socket = isl_flmap[param_name]
 
-            if input_socket not in vis:
+            if isl_flmap[param_name] not in vis:
                 continue
 
-            ## position the text rect horizontally,
-            ## 10 pixels from the left side of the
-            ## node's left (which you can retrieve
-            ## from the top rectsman)
-            text_rect.left = top_rectsman.left + 10
+            ## draw
 
-            ## position the text rect vertically
+            text_obj = pto_map[param_name]
+            text_rect = text_obj.rect
 
-            # the text is positioned vertically centered on the
-            # input socket, lifted just 2 pixels up
-            text_rect.centery = input_socket.rect.move(0, -2).centery
+            _topleft = text_rect.topleft
 
-            ## define text
-            text = param_name
+            text_rect.move_ip(offset)
+
+            text_obj.draw_on_surf(body_surf)
+
+            text_rect.topleft = _topleft
+
 
         ## otherwise, we have a parameter of variable kind
         ## here...
@@ -150,9 +189,13 @@ def get_collapsed_body_surface(self):
         else:
 
             ## retrieve the names of the subparameters sorted
-            sorted_subparam_indices = sorted(self.input_socket_live_flmap[param_name])
+
+            sorted_subparam_indices = (
+                sorted(self.input_socket_live_flmap[param_name])
+            )
 
             ## skip parameter if it has no subparameters
+
             if not sorted_subparam_indices:
                 continue
 
@@ -166,193 +209,105 @@ def get_collapsed_body_surface(self):
             else:
                 continue
 
+            ## draw text object
+
+            text_obj = pto_map[param_name]
+            text_rect = text_obj.rect
+
+            _topleft = text_rect.topleft
+
+            text_rect.move_ip(offset)
+
+            text_obj.draw_on_surf(body_surf)
+
+            text_rect.topleft = _topleft
+
             ## if the subparameter for the visible socket is marked
             ## for unpacking, draw the unpacking icon beside the socket
 
-            for subparam_index in sorted_subparam_indices:
+#            for subparam_index in sorted_subparam_indices:
+#
+#                socket = isl_flmap[param_name][subparam_index]
+#
+#                if socket not in vis:
+#                    continue
+#
+#                unpacking_icon = sui_flmap[param_name].get(subparam_index)
+#
+#                if not unpacking_icon:
+#                    continue
+#
+#                icon_rect = unpacking_icon.rect
+#
+#                _topleft = icon_rect.topleft
+#
+#                icon_rect.move_ip(offset)
+#
+#                unpacking_icon.draw_on_surf(body_surf)
+#
+#                icon_rect.topleft = _topleft
 
-                socket = isl_flmap[param_name][subparam_index]
-
-                if socket not in vis:
-                    continue
-
-                unpacking_icon = sui_flmap[param_name].get(subparam_index)
-
-                if not unpacking_icon:
-                    continue
-
-                unpacking_icon.rect.move_ip(offset)
-                unpacking_icon.draw_on_surf(body_surf)
-                unpacking_icon.rect.move_ip(self.body.rect.topleft)
-
-            ## position the text rect horizontally,
-            ## 5 pixels from the left side of the node's
-            ## left (which you can retrieve from the
-            ## top rectsman)
-            text_rect.left = top_rectsman.left + 5
-
-            ## position vertically
-            text_rect.bottom = param_rectsman.move(0, -2).top
-
-            ## define text (instead of just using the
-            ## parameter's name, we add one or two
-            ## additional asterisks, to indicate that
-            ## the parameter is of variable kind and
-            ## the specific variable kind)
-
-            asterisks = "*" if var_kind == "var_pos" else "**"
-
-            text = asterisks + param_name
-
-        ### define coordinates info
-
-        coordinates_name = "topleft"
-        coordinates_value = text_rect.topleft
-
-        ### instantiate positioned text object
-
-        text_obj = Object2D.from_surface(
-            surface=render_text(
-                text=text,
-                ## text settings
-                font_height=APP_REFS.general_font_height,
-                foreground_color=NODE_LABELS,
-                background_color=node_light_bg_color,
-            ),
-            coordinates_name=coordinates_name,
-            coordinates_value=coordinates_value,
-        )
-
-        ### reposition the object relative to the
-        ### surface's origin and blit the obj's surface
-        ### on the body surface
-
-        text_obj.rect.move_ip(offset)
-        text_obj.draw_on_surf(body_surf)
-
-    ### blit labels on the body surface, beside each output
-    ### socket so the name of each output socket is visible
-    ### on the node
-
-    ## retrieve name of socket in the order they appear
-    ordered_socket_names = self.ordered_output_type_map.keys()
-
-    ## retrieve map wherein to find the output sockets
-    osl_map = self.output_socket_live_map
-
-    ## position the text rect 10 pixels to the left of
-    ## the right side of the node (we use the right of
-    ## the top_rectsman)
-    text_rect.right = top_rectsman.right - 10
-
-    ## also assign 'topright' as the name of
-    ## the coordinates from where we'll position
-    ## the text objects we'll be creating
-    coordinates_name = "topright"
-
-    ## iterate over names of output sockets;
-    ##
-    ## for those with children create the text surface
-    ## for their respective names and blit them in the
-    ## body surface
-
-    for output_socket_name in ordered_socket_names:
-
-        ## grab the output socket
-        output_socket = osl_map[output_socket_name]
-
-        ## if the socket has children, skip
-
-        has_parent = (
-            (self.id, output_socket_name) in APP_REFS.gm.parent_sockets_ids
-            if hasattr(APP_REFS.gm, 'parent_sockets_ids')
-            else hasattr(output_socket, 'children')
-        )
-
-        if not has_parent:
-            continue
-
-        ## align the text rect vertically with the
-        ## output socket and lift the text rect 2 pixels;
-        ##
-        ## we don't need to do any more positioning
-        ## than this, since the sockets are already
-        ## positioned suitably (they were already
-        ## positioned on the self.reposition_elements
-        ## method relative to the text);
-        ##
-        ## here we just want to get the suitable position
-        ## to blit the text
-
-        text_rect.centery = output_socket.rect.centery
-        text_rect.top += -2
-
-        ## grab the topright coordinate from the
-        ## text rect
-        coordinates_value = text_rect.topright
-
-        ## instantiate the text object in the given position
-
-        text_obj = Object2D.from_surface(
-            surface=(
-                render_text(
-                    text=output_socket_name,
-                    font_height=APP_REFS.general_font_height,
-                    foreground_color=NODE_LABELS,
-                    background_color=(node_light_bg_color),
-                )
-            ),
-            coordinates_name=coordinates_name,
-            coordinates_value=coordinates_value,
-        )
-
-        ## reposition the object relative to the
-        ## surface's origin and blit the obj's surface
-        ## on the body surface
-
-        text_obj.rect.move_ip(offset)
-        text_obj.draw_on_surf(body_surf)
 
     ### outline the sides of the body surface
 
     ## define lines represented by pairs of points
 
-    # calculate and store the result from subtracting
-    # the outline thickness from the body width
-    # (which is equivalent to the NODE_WIDTH (184));
+    # calculate and store the result from subtracting the outline thickness
+    # from the body width
     #
-    # we'll call this the 'offset_width', and it is
-    # the x coordinate from where we'll define the points
-    # of the line on the right side of the node;
+    # we'll call this the 'offset_width', and it is the x coordinate from where
+    # we'll define the points of the line on the right side of the node;
     #
-    # this is needed because the thickness of the lines
-    # are blitted from left to right and thus
-    # wouldn't appear if the line were placed
-    # right on top of the right side; instead, it must be
-    # offset to the left by subtracting the thickness
-    offset_width = 184 - NODE_OUTLINE_THICKNESS
+    # this is needed because the thickness of the lines are blitted from left
+    # to right and thus wouldn't appear if the line were placed right on top of
+    # the right side; instead, it must be offset to the left by subtracting the
+    # thickness
+    offset_width = self.body.rect.width - NODE_OUTLINE_THICKNESS
 
     # defining lines
 
     lines = (
+
         # line on left side
         ((0, 0), (0, body_height)),
+
         # line on right side
         ((offset_width, 0), (offset_width, body_height)),
+
     )
 
     # drawing lines
 
     for point_a, point_b in lines:
 
-        draw_line(body_surf, NODE_OUTLINE, point_a, point_b, NODE_OUTLINE_THICKNESS)
+        draw_line(
+            body_surf,
+            NODE_OUTLINE,
+            point_a,
+            point_b,
+            NODE_OUTLINE_THICKNESS,
+        )
 
     ### separate body head from rest of the body by drawing
     ### a horizontal line between those areas
 
     line_y = BODY_HEAD_SURF.get_height()
 
-    draw_line(body_surf, NODE_OUTLINE, (0, line_y), (body_surf.get_width(), line_y), 2)
+    draw_line(
+
+        body_surf,
+
+        # line color
+        NODE_OUTLINE,
+
+        # line start, end
+        (0, line_y),
+        (body_surf.get_width(), line_y),
+
+        # width
+        2,
+
+    )
 
     ### if there's a keyword-variable parameter in the
     ### node, only for the subparameters whose input
@@ -367,9 +322,13 @@ def get_collapsed_body_surface(self):
 
     if "var_key" in self.var_kind_map.values():
 
-
         param_name = next(
-            key for key, value in self.var_kind_map.items() if value == "var_key"
+
+            key
+
+            for key, value in self.var_kind_map.items()
+            if value == "var_key"
+
         )
 
         subparam_input_sockets_map = isl_flmap[param_name]
@@ -382,27 +341,30 @@ def get_collapsed_body_surface(self):
             if subparam_input_sockets_map[subparam_index] not in vis:
                 continue
 
-            ## obtain the midleft coordinates of the
-            ## object (a bit offset to left), but
-            ## changed so that it is relative to the origin
-            ## of the body surface
+            ## obtain the midleft coordinates of the object
+            ## (a bit offset to left), but changed so that it is relative
+            ## to the origin of the body surface
 
             x, y = (
+
                 # get object's rect
                 obj.rect
+
                 # get new rect moved a bit to the left
                 .move(-2, 0)
+
                 # and yet another one moved from there so its
                 # position is relative to the origin of the
                 # body surface
                 .move(offset)
+
                 # then grab its midleft coordinates
                 .midleft
+
             )
 
-            ## assign the midleft coordinates calculated
-            ## to the midright coordinates of
-            ## the keyword key's rect, then blit it in that
+            ## assign the midleft coordinates calculated to the midright
+            ## coordinates of the keyword key's rect, then blit it in that
             ## position
 
             KEYWORD_KEY_RECT.midright = x, y
@@ -411,6 +373,7 @@ def get_collapsed_body_surface(self):
                 KEYWORD_KEY_SURF,
                 KEYWORD_KEY_RECT,
             )
+
 
     ### finally return the body surface
     return body_surf
--- nodezator/graphman/callablenode/vizprep/bodysetup/expandedcreation.py
+++ nodezator/graphman/callablenode/vizprep/bodysetup/expandedcreation.py
@@ -25,8 +25,6 @@ from ...constants import NODE_OUTLINE_THICKNESS
 
 from .....colorsman.colors import (
     NODE_OUTLINE,
-    NODE_BODY_BG,
-    COMMENTED_OUT_NODE_BG,
     NODE_LABELS,
 )
 
@@ -41,8 +39,7 @@ def get_expanded_body_surface(self):
     ### reference the body's height locally
     body_height = self.body.rect.height
 
-    ### create a surface for the body of the node
-
+    ### reference surface for the body of the node
     body_surf = self.body.image
 
     ### obtain body head surf from corresponding map
@@ -79,18 +76,6 @@ def get_expanded_body_surface(self):
 
     )
 
-    ### define color used for node bg, depending on whether the node is
-    ### commented out or not
-
-    node_bg_color = (
-
-        COMMENTED_OUT_NODE_BG
-        if self.data.get('commented_out', False)
-
-        else NODE_BODY_BG
-
-    )
-
     ### offset for blitting surfaces onto body's surf
     offset = tuple(-coordinate for coordinate in self.body.rect.topleft)
 
@@ -102,8 +87,8 @@ def get_expanded_body_surface(self):
     ## reference the output text object map locally
     oto_map = self.output_text_obj_map
 
-    ## iterate over names of output names, blitting the text surface
-    ## for each name on the body surface
+    ## iterate over names of outputs, blitting the text surface for each name
+    ## on the body surface
 
     for output_name in ordered_output_names:
 
@@ -135,7 +120,7 @@ def get_expanded_body_surface(self):
 
         _topleft = text_rect.topleft
 
-        text_obj.rect.move_ip(offset)
+        text_rect.move_ip(offset)
 
         text_obj.draw_on_surf(body_surf)
 
@@ -146,18 +131,16 @@ def get_expanded_body_surface(self):
 
     ## define lines represented by pairs of points
 
-    # calculate and store the result from subtracting
-    # the outline thickness from the body width
+    # calculate and store the result from subtracting the outline thickness
+    # from the body width
     #
-    # we'll call this the 'offset_width', and it is
-    # the x coordinate from where we'll define the points
-    # of the line on the right side of the node;
+    # we'll call this the 'offset_width', and it is the x coordinate from where
+    # we'll define the points of the line on the right side of the node;
     #
-    # this is needed because the thickness of the lines
-    # are blitted from left to right and thus
-    # wouldn't appear if the line were placed
-    # right on top of the right side; instead, it must be
-    # offset to the left by subtracting the thickness
+    # this is needed because the thickness of the lines are blitted from left
+    # to right and thus wouldn't appear if the line were placed right on top of
+    # the right side; instead, it must be offset to the left by subtracting the
+    # thickness
     offset_width = self.body.rect.width - NODE_OUTLINE_THICKNESS
 
     # defining lines
@@ -205,21 +188,22 @@ def get_expanded_body_surface(self):
 
     )
 
-    ### if there's a keyword-variable parameter in the
-    ### node, blit the keyword key icon beside each
-    ### keyword entry or subparameter unpacking icon
-    ### for that parameter (if there's any subparameter);
+    ### if there's a keyword-variable parameter in the node, blit the keyword
+    ### key icon beside each keyword entry or subparameter unpacking icon for
+    ### that parameter (if there's any subparameter);
     ###
-    ### the motivation is purely aesthetic, but
-    ### from my experience the effect improves readability,
-    ### since it makes it easier to spot keyword entry
-    ### widgets among other entry widgets the node might
-    ### be using
+    ### the motivation is purely aesthetic, but from my experience the effect
+    ### improves readability, since it makes it easier to spot keyword entry
+    ### widgets among other entry widgets the node might be using
 
-    if "var_key" in self.var_kind_map.values():
+    if 'var_key' in self.var_kind_map.values():
 
         param_name = next(
-            key for key, value in self.var_kind_map.items() if value == "var_key"
+
+            key
+
+            for key, value in self.var_kind_map.items()
+            if value == 'var_key'
         )
 
         for obj in chain(
@@ -227,27 +211,29 @@ def get_expanded_body_surface(self):
             self.subparam_unpacking_icon_flmap[param_name].values(),
         ):
 
-            ## obtain the midleft coordinates of the
-            ## object (a bit offset to left), but
-            ## changed so that it is relative to the origin
-            ## of the body surface
+            ## obtain the midleft coordinates of the object
+            ## (a bit offset to left), but changed so that it is relative
+            ## to the origin of the body surface
 
             x, y = (
+
                 # get object's rect
                 obj.rect
+
                 # get new rect moved a bit to the left
                 .move(-2, 0)
+
                 # and yet another one moved from there so its
                 # position is relative to the origin of the
                 # body surface
                 .move(offset)
+
                 # then grab its midleft coordinates
                 .midleft
             )
 
-            ## assign the midleft coordinates calculated
-            ## to the midright coordinates of
-            ## the keyword key's rect, then blit it in that
+            ## assign the midleft coordinates calculated to the midright
+            ## coordinates of the keyword key's rect, then blit it in that
             ## position
 
             KEYWORD_KEY_RECT.midright = x, y
@@ -256,6 +242,7 @@ def get_expanded_body_surface(self):
                 KEYWORD_KEY_SURF,
                 KEYWORD_KEY_RECT,
             )
+
 
     ### finally return the body surface
     return body_surf
